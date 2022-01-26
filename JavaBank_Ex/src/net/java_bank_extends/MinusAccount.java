9ff058c8b9bc384639514eccc064f927eba91b0e
Jeongc2022 <jeongc2022@gmail.com>
2022-01-26 09:39:06 +0900

    java_bank_extends answer

---
+++ JavaBank_Ex/src/net/java_bank_extends/MinusAccount.java
@@ -0,0 +1,38 @@
+package net.java_bank_extends;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class MinusAccount extends Account {
+	static final String KIND = "마이너스";	// 마이너스 계좌
+	
+	public MinusAccount() {}
+	
+	public MinusAccount(String accountNo, String name) {
+		super(accountNo, name);
+	}
+	
+	public MinusAccount(String accountNo, String name, double balance) {
+		super(accountNo, name, balance );
+	}
+	
+	@Override
+	public String getKind() {
+		return KIND;
+	}
+	
+	public void withdraw(double amount) {
+		balance = balance - amount;
+		Transaction transaction = new Transaction();
+		Calendar cal = Calendar.getInstance();
+		Date date = cal.getTime();
+		transaction.setTransactionDate(Account.DATE_FORMAT.format(date));
+		transaction.setTransactionTime(Account.TIME_FORMAT.format(date));
+		transaction.setAmount(amount);
+		transaction.setBalance(balance);
+		transaction.setKind(Account.WITHDRAW);
+		transactions.add(transaction);
+		
+	}
+
+}
